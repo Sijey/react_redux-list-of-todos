a865cd0379181b71f496ca93de16c4e56f0bb581
Sijey <Popov040391@gmail.com>
2019-11-25 16:22:25 +0200

    Updates

--- precache-manifest.1f311354915eef542678f585092b4091.js
+++ precache-manifest.1f311354915eef542678f585092b4091.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3381185820d2574632aefcc55c90d852",
+    "revision": "d75e4cc51168669439adff1a56bba243",
     "url": "/react_redux-list-of-todos/index.html"
   },
   {
-    "revision": "b13138bbeb8ac7f53b7c",
+    "revision": "316124796762075b0b4e",
     "url": "/react_redux-list-of-todos/static/css/main.cb9e985a.chunk.css"
   },
   {
-    "revision": "e24e4825e0d39b619b38",
-    "url": "/react_redux-list-of-todos/static/js/2.6a15398a.chunk.js"
+    "revision": "0405bcb47497ac826355",
+    "url": "/react_redux-list-of-todos/static/js/2.18d5d753.chunk.js"
   },
   {
-    "revision": "b13138bbeb8ac7f53b7c",
-    "url": "/react_redux-list-of-todos/static/js/main.d2369cc8.chunk.js"
+    "revision": "316124796762075b0b4e",
+    "url": "/react_redux-list-of-todos/static/js/main.b1a4d6bb.chunk.js"
   },
   {
     "revision": "37dc44999646af065e8e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_redux-list-of-todos/precache-manifest.736a56ea285976f080e41f12d45d5fde.js"
+  "/react_redux-list-of-todos/precache-manifest.1f311354915eef542678f585092b4091.js"
 );
 
 self.addEventListener('message', (event) => {
